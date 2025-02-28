a4fec1ba27a3a19d82937bbed82283b4ab8807df
OnlySid07 <74305606@certus.edu.pe>
2025-02-28 15:03:49 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,40 @@
+const productos = [
+    { id: 1, nombre: 'Producto 1', precio: 100 },
+];
+
+let carrito = [];
+
+document.addEventListener('DOMContentLoaded', mostrarProductos);
+
+//Mostrar los productos en el HTML con el div.innerhtml
+function mostrarProductos(){
+    const contenedor = document.getElementById("products");
+    productos.forEach(producto => {
+    let div = document.createElement("div");
+    div.classList.add("product");
+    div.innerHTML = '<h3>' + producto.nombre + '</h3>' + 
+    '<p>' + producto.precio + '</p>';
+    contenedor.appendChild(div);
+
+});
+}
+
+function agregarCarrito (id){
+
+}
+
+
+
+
+
+
+// Es para mostrar mi lista de compras
+
+function actualizarCarrito(){
+    const listaCarrito = document.getElementById("listaCarrito");
+    const totalElmento = document.getElementById("total")
+
+    listaCarrito.innerHTML = '';
+
+    let total = 0; 
+}
