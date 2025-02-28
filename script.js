b8dc9cad7b4d4cffcb1053eaf133714122c18dd5
OnlySid07 <74305606@certus.edu.pe>
2025-02-28 15:12:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ function mostrarProductos(){
     div.classList.add("product");
     div.innerHTML = '<h3>' + producto.nombre + '</h3>' + 
     '<p>' + producto.precio + '</p>'+
-    Image('<img src="https://upload.wikimedia.org/wikipedia/commons/1/11/Promart_logo_2019.svg">') ;
+    '<img src="https://upload.wikimedia.org/wikipedia/commons/1/11/Promart_logo_2019.svg">'
     contenedor.appendChild(div);
 
 });
